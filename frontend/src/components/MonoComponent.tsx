dd7ed0bd76119b125b12a4e070da1665bd04fd2b
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2026-03-21 09:34:29 +0100

    updated mono component

--- frontend/src/components/MonoComponent.tsx
+++ frontend/src/components/MonoComponent.tsx
@@ -6,27 +6,39 @@ const monoURL: string = import.meta.env.VITE_MONO_PUBLIC_KEY;
 
 const MonoComponent: React.FC = () => {
   const [monoInstance, setMonoInstance] = useState<any>(null);
+  const [linked, setLinked] = useState<boolean>(false);
 
   useEffect(() => {
     const mono = new Connect({
       key: monoURL,
+
       onSuccess: async (data: MonoSuccessData) => {
         await axiosClient.post("/expense/exchangeCode", {
           code: data.code,
         });
+        setLinked(true);
       },
+
       onClose: () => console.log("User closed the widget without connecting"),
     });
 
     mono.setup();
 
     setMonoInstance(mono);
+
+    return () => {
+      mono.close();
+    };
   }, []);
   return (
     <div>
-      <button onClick={() => monoInstance?.open()} className="cursor-pointer ">
-        Link Bank Account
-      </button>
+      {linked ? (
+        <p>Bank account linked successfully</p>
+      ) : (
+        <button onClick={() => monoInstance?.open()} className="cursor-pointer">
+          Link bank account
+        </button>
+      )}
     </div>
   );
 };
